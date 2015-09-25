75456b5ea41fb8444dabbaa9fc667c5ef2b44cfb
Michael Howitz <mh@gocept.com>
2015-09-25 10:58:45 +0200

    Rewrote tests to be real py.test tests

--- src/gocept/loginuser/tests/test_password.py
+++ src/gocept/loginuser/tests/test_password.py
@@ -1,40 +1,39 @@
+import gocept.loginuser.password
+import gocept.testing.patch
 import hashlib
+import pytest
 import six
-import unittest
 
 
-class PasswordHashingTest(unittest.TestCase):
+@pytest.yield_fixture(scope='module', autouse=True)
+def patches():
+    """Patch bcrypt WORK_FACTOR to speed up tests."""
+    patch = gocept.testing.patch.Patches()
+    patch.set(gocept.loginuser.password, 'WORK_FACTOR', 4)
+    yield
+    patch.reset()
 
-    def setUp(self):
-        import gocept.loginuser.password
-        import gocept.testing.patch
-        self.patch = gocept.testing.patch.Patches()
-        self.patch.set(gocept.loginuser.password, 'WORK_FACTOR', 4)
 
-    def tearDown(self):
-        self.patch.reset()
+def test_password__check__1():
+    """`check()` verifies a password."""
+    hashed = gocept.loginuser.password.hash('mypassword')
+    assert gocept.loginuser.password.check('mypassword', hashed)
+    assert not gocept.loginuser.password.check('invalid', hashed)
 
-    def test_verifying_passwords(self):
-        import gocept.loginuser.password
-        hashed = gocept.loginuser.password.hash('mypassword')
-        self.assertTrue(gocept.loginuser.password.check(
-            'mypassword', hashed))
-        self.assertFalse(gocept.loginuser.password.check(
-            'invalid', hashed))
 
-    def test_hash_accepts_and_returns_unicode(self):
-        import gocept.loginuser.password
-        hashed = gocept.loginuser.password.hash(u'mypassword')
-        self.assertIsInstance(hashed, six.text_type)
+def test_password__check__2():
+    """`check()` defaults to bcrypt."""
+    hashed = gocept.loginuser.password.hash(u'asdf')
+    assert gocept.loginuser.password.check('asdf', hashed)
 
-    def test_sha256_password_is_recognised(self):
-        import gocept.loginuser.password
-        hashed = hashlib.sha256(b'asdf').hexdigest()
-        self.assertTrue(gocept.loginuser.password.check(
-            'asdf', 'sha256:' + hashed))
 
-    def test_password_check_defaults_to_bcrypt(self):
-        import gocept.loginuser.password
-        hashed = gocept.loginuser.password.hash(u'asdf')
-        self.assertTrue(gocept.loginuser.password.check(
-            'asdf', hashed))
+def test_password__check__3():
+    """`check()` recognizes sha256:<hash>."""
+    hashed = hashlib.sha256(b'asdf').hexdigest()
+    assert gocept.loginuser.password.check('asdf', 'sha256:' + hashed)
+
+
+def test_password__hash__1():
+    """`hash()` accepts and returns unicode."""
+    hashed = gocept.loginuser.password.hash(u'mypassword')
+    assert isinstance(hashed, six.text_type)
